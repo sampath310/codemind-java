89b94f3620b9519c9d5b16fdd0bff5ca31764a52
sampath <84090760+sampath310@users.noreply.github.com>
2022-09-13 11:29:05 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n=sc.nextInt();         int k=sc.nextInt();         int arr[]=new int[n];         int count=0;         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();             if(arr[i]%k!=0)                 count++;         }         System.out.println(count);      } }

---
+++ Count_elements_not_divisible_by_K.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int arr[]=new int[n];
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(arr[i]%k!=0)
+                count++;
+        }
+        System.out.println(count);
+     }
+}
